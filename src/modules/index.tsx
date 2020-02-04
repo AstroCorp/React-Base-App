1c44b3de2312de41f228f33fc7b4aa33609db907
Alejandro Leal Bermúdez <aleaberm12@gmail.com>
2020-02-04 19:35:08 +0100

    Update index.tsx

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
-import counter from './counter';
 import { History } from 'history';
+import counter from './counter';
 
 export default (history: History) => combineReducers({
   router: connectRouter(history),
