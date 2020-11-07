43280465d4aea60100f404c9b58a1ddede030316
Alejandro Leal <aleaberm12@gmail.com>
2020-11-07 01:44:21 +0100

    Dependencias actualizadas

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-const TailwindVueExtractor = content => {
+const TailwindExtractor = content => {
     const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, "");
     return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_:/]+/g) || [];
 };
@@ -10,17 +10,17 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     content: [`./@(public|src)/**/*.@(${extensionsUsingCSS.join("|")})`],
     css: [`./src/**/*.@(${extensionsOfCSS.join("|")})`],
     extractors: [
-        {
-            extractor: TailwindVueExtractor,
-            extensions: extensionsUsingCSS,
-        },
+      {
+        extractor: TailwindExtractor,
+        extensions: extensionsUsingCSS,
+      },
     ],
 });
   
-  module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-      ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
-    ],
-  };
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
+};
